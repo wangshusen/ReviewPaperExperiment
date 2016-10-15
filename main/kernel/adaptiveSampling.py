1abcecdb0f145afb46695dca74b69bb6dcce2241
Shusen Wang <wssatzju@gmail.com>
2016-10-14 21:12:04 -0700

    changed the adaptive sampling

--- main/kernel/adaptiveSampling.py
+++ main/kernel/adaptiveSampling.py
@@ -3,26 +3,46 @@ import kernelFun
 
 def adaptiveSamplingRBF(matX, sigma, s):
     n = matX.shape[0]
-    s1 = int(numpy.ceil(s/3))
-    n0 = min(n, 5 * s) # the oversampling parameter can be tuned
+    s1 = int(numpy.floor(s/3))
+    s2 = s1
+    s3 = s - s1 - s2
+    n0 = min(n, 10 * s) # the oversampling parameter can be tuned
     # ================== Over-Sampling ================== #
     arrIdx0 = numpy.random.choice(n, n0, replace=False)
     matA = kernelFun.rbf(matX, matX[arrIdx0, :], sigma)
     # ================ Uniform Sampling ================ #
     arrIdx1 = list(range(s1))
-    matC1 = matA[:, arrIdx1]# compute the residual
+    matC1 = matA[:, arrIdx1]
     matQ = numpy.linalg.qr(matC1, mode='reduced')[0]
     del matC1
-    # ================ Compute Residual ================ #
+    # ================ Adaptive Sampling 1 ================ #
+    # Compute Residual
     matRes = numpy.dot(matQ, numpy.dot(matQ.T, matA))
     matRes = matA - matRes
-    # ==================== Sampling ==================== #
+    # Sampling
     matRes = numpy.square(matRes)
     vecProb = sum(matRes)
     del matRes
     vecProb = vecProb / sum(vecProb)
-    arrIdx2 = numpy.random.choice(n0, s-s1, replace=False, p=vecProb)
-    # ==================== Combine ==================== #
+    arrIdx2 = numpy.random.choice(n0, s2, replace=False, p=vecProb)
+    # Combine
     arrIdx = numpy.concatenate((arrIdx1, arrIdx2))
+    matC2 = matA[:, arrIdx]
+    matQ = numpy.linalg.qr(matC2, mode='reduced')[0]
+    del matC2
+    # ================ Adaptive Sampling 2 ================ #
+    # Compute Residual
+    matRes = numpy.dot(matQ, numpy.dot(matQ.T, matA))
+    del matQ
+    matRes = matA - matRes
+    # Sampling
+    matRes = numpy.square(matRes)
+    vecProb = sum(matRes)
+    del matRes
+    vecProb = vecProb / sum(vecProb)
+    arrIdx3 = numpy.random.choice(n0, s3, replace=False, p=vecProb)
+    # Combine
+    arrIdx = numpy.concatenate((arrIdx, arrIdx3))
+    
     arrIndex = numpy.unique(arrIdx0[arrIdx])
     return arrIndex
